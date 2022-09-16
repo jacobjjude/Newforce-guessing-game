1c7d86332b5fe702369ca9639d6662b9ea28c03d
Jacob Jude <jacobj.jude@outlook.com>
2022-09-16 14:35:42 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace guessing_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare variables, store.
+            string userInput;
+            int intVal;
+            Console.WriteLine("Guess the secret number: ");
+            userInput = Console.ReadLine();
+            //convert to integer
+            intVal = Convert.ToInt32(userInput);
+            Console.WriteLine($"You entered {intVal}");
+        }
+    }
+}
